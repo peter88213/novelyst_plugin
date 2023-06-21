39fd72c7eceec395a4c93d7f39756217f5e4467a
peter88213 <peter.88213@t-online.de>
2023-06-21 16:10:41 +0200

    Update novelyst_plugin.py

--- src/novelyst_plugin.py
+++ src/novelyst_plugin.py
@@ -13,6 +13,21 @@ from tkinter import messagebox
 
 APPLICATION = 'Example plugin'
 
+# Initialize localization.
+LOCALE_PATH = f'{os.path.dirname(sys.argv[0])}/locale/'
+try:
+    CURRENT_LANGUAGE = locale.getlocale()[0][:2]
+except:
+    # Fallback for old Windows versions.
+    CURRENT_LANGUAGE = locale.getdefaultlocale()[0][:2]
+try:
+    t = gettext.translation('novelyst_plugin', LOCALE_PATH, languages=[CURRENT_LANGUAGE])
+    _ = t.gettext
+except:
+
+    def _(message):
+        return message
+
 
 class Plugin:
     """Example plugin class.
